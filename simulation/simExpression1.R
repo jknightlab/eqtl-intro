3a51496b3805fbb3c3dad139278912e37f001664
Peter Humburg <humburg@well.ox.ac.uk>
2015-03-14 21:59:48 +0000

    Added script to generate very simple expression data.

---
+++ simulation/simExpression1.R
@@ -0,0 +1,9 @@
+## Simulate gene expression for one gene per SNP.
+## Expression is base + snp + noise
+set.seed(101)
+snp <- readr::read_tsv("output/sim_genotypes.tab")
+errorMeans <- rnorm(nrow(snp))
+expr <- sapply(snp[-1], function(g) 7 + 1.5*g) + 
+		matrix(rnorm(nrow(snp)*(ncol(snp)-1), mean=errorMeans, sd=0.5), nrow=nrow(snp))
+expr <- data.frame(snp$sample, expr)
+write.table(expr, file="output/sim_expression1.tab", sep="\t", quote=FALSE, row.names=FALSE)
