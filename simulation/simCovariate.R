d919642a5019e714eaff0279b3b94a3e68d30e76
Peter Humburg <humburg@well.ox.ac.uk>
2015-03-15 09:17:22 +0000

    Use covariates with mean 0.

--- simulation/simCovariate.R
+++ simulation/simCovariate.R
@@ -2,7 +2,7 @@
 ## gene expression data.
 set.seed(102)
 n <- 300; nc <- 20
-cvrt <- matrix(rnorm(n*nc, mean=1), ncol = nc)
+cvrt <- matrix(rnorm(n*nc), ncol = nc)
 cvrt <- data.frame(paste("sample", 1:n, sep="_"), cvrt)
 names(cvrt) <- c("sample", paste("var", 1:nc, sep="_"))
 write.table(cvrt, file="output/sim_covariates.tab", sep="\t", quote=FALSE, row.names=FALSE)
