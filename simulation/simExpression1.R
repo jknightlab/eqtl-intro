391e333743ff631604e4c6dbbc77794563a1fb49
Peter Humburg <humburg@well.ox.ac.uk>
2015-03-15 08:12:42 +0000

    Changed column names in output file.

--- simulation/simExpression1.R
+++ simulation/simExpression1.R
@@ -6,4 +6,5 @@ errorMeans <- rnorm(nrow(snp))
 expr <- sapply(snp[-1], function(g) 7 + 1.5*g) + 
 		matrix(rnorm(nrow(snp)*(ncol(snp)-1), mean=errorMeans, sd=0.5), nrow=nrow(snp))
 expr <- data.frame(snp$sample, expr)
+names(expr) <- c("sample", paste("gene", 1:(ncol(expr)-1), sep="_"))
 write.table(expr, file="output/sim_expression1.tab", sep="\t", quote=FALSE, row.names=FALSE)
